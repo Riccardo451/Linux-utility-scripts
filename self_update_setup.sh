6c0548c3269b99731ee4625e0793cd078cb42866
Riccardo451 <ricky451@hotmail.it>
2020-02-19 13:02:17 +0100

    working setup

---
+++ self_update_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mkdir /home/pi/Update
+
+echo "#!/bin/sh
+# redirect stdout/stderr to a file
+exec 1>>/home/pi/Update/logfile.txt 2>&1
+echo '----------------------------------'
+echo '           '
+date -u
+echo '           '
+sudo apt update
+sudo apt -yf dist-upgrade
+sudo apt upgrade -y
+sudo apt -y --purge autoremove
+sudo apt autoclean
+#pihole -g -up
+echo 'Raspberry updated now rebooting'
+echo '----------------------------------'
+sudo reboot
+" > /home/pi/Update/update.sh
+
+sudo chmod +x /home/pi/Update/update.sh
+
+sudo crontab -l 2>>/dev/null | { cat; echo "0 5 * * 1 /home/pi/Update/update.sh"; } | sudo crontab -
